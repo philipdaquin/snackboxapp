1f34479ab5c4db4206929194bce3367cf79247dd
philipdaquin <philipdaquin20@gmail.com>
2022-08-08 18:44:48 +0800

    carog toml env

--- app/App.js
+++ app/App.js
@@ -9,12 +9,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
