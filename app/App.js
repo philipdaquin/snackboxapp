cf7af8054feb3778c8186c61b75b53090d01eae8
philipdaquin <philipdaquin20@gmail.com>
2022-08-08 19:14:40 +0800

    installed dependencies

--- app/App.js
+++ app/App.js
@@ -9,3 +9,12 @@ export default function App() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
