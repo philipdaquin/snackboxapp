0854a762895190143eebe805ebc98769680b545e
philipdaquin <philipdaquin20@gmail.com>
2022-08-11 11:01:39 +0800

    account screen

--- app/Navigation/BottomTab.js
+++ app/Navigation/BottomTab.js
@@ -3,12 +3,22 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import {HomeIcon, SearchIcon, NewspaperIcon, UserIcon, TagIcon} from 'react-native-heroicons/outline'
+// import {HomeIcon, SearchIcon, NewspaperIcon, UserIcon, TagIcon} from 'react-native-heroicons/solid'
 import DealsScreen from '../screens/DealsScreen';
 import OrderScreen from '../screens/OrderScreen';
 import AccountScreen from '../screens/AccountScreen';
 import SearchScreen from '../screens/SearchScreen';
-const Tab = createBottomTabNavigator();
 
+import {
+  HomeIcon as HomeFocus, 
+  SearchIcon as SearchFocus, 
+  NewspaperIcon as NewspaperFocus, 
+  UserIcon as UserFocus, 
+  TagIcon as TagFocus
+} from 'react-native-heroicons/solid'
+
+
+const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
   return (
@@ -20,7 +30,6 @@ const BottomTab = () => {
         tabBarInactiveTintColor: 'black',
         tabBarIconStyle: {
             padding: 14,
-            
         },
         tabBarLabelStyle: { 
             fontSize: 13,
@@ -36,20 +45,52 @@ const BottomTab = () => {
         
     }}>
       <Tab.Screen name="Home" component={HomeScreen} options={{
-        tabBarIcon: () => (<HomeIcon size={35} strokeWidth={1.5} color={"#000"} />   )
-      }} />
+        tabBarIcon: ({focused}) => (
+            focused ? (
+              <HomeFocus size={35} strokeWidth={1.5} color={"#000"} />
+
+            ) : (
+              <HomeIcon size={35} strokeWidth={1.5} color={"#000"} />
+            ) 
+          )
+      }} /> 
       <Tab.Screen name="Search" component={SearchScreen} options={{
-        tabBarIcon: () => (<SearchIcon size={35} strokeWidth={1.5} color={"#000"} />   )
+        tabBarIcon: ({focused}) => (
+            focused ? (
+              <SearchFocus size={35} strokeWidth={1.5} color={"#000"} />
+            ) : (
+              <SearchIcon size={35} strokeWidth={1.5} color={"#000"} />
+
+            )
+          )
       }} />
       <Tab.Screen name="Orders" component={OrderScreen} options={{
-        tabBarIcon: () => (<NewspaperIcon size={35} strokeWidth={1.5} color={"#000"} />   )
+        tabBarIcon: ({focused}) => (
+          focused ? (
+            <NewspaperFocus size={35} strokeWidth={1.5} color={"#000"} />   
+          ) : (
+            <NewspaperIcon size={35} strokeWidth={1.5} color={"#000"} />   
+          )
+        )
       }} />
       <Tab.Screen name="Today" component={DealsScreen} options={{
-        tabBarIcon: () => (<TagIcon size={35} strokeWidth={1.5} color={"#000"} />   )
+        tabBarIcon: ({focused}) => (
+            focused ? (
+              <TagFocus size={35} strokeWidth={1.5} color={"#000"} />   
+            ): (
+              <TagIcon size={35} strokeWidth={1.5} color={"#000"} />   
+            ) 
+          )
       }} />
       
       <Tab.Screen name="Accounts" component={AccountScreen} options={{
-        tabBarIcon: () => (<UserIcon size={35} strokeWidth={1.5} color={"#000"} />   )
+        tabBarIcon: ({focused}) => (
+            focused ? (
+              <UserFocus size={35} strokeWidth={1.5} color={"#000"} />   
+            ): ( 
+              <UserIcon size={35} strokeWidth={1.5} color={"#000"} />   
+            )
+          )
       }} />
     </Tab.Navigator>
   )
--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,11 +1,83 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import { ChevronRightIcon } from 'react-native-heroicons/outline' 
 const AccountScreen = () => {
   return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
+    // <ScrollView>
+      <SafeAreaView className="bg-white pt-16 px-4 pb-44">
+        
+        {/* Header */}
+        <View>
+          <Text className="font-medium text-left text-3xl">Account</Text>
+          <View className="border-y border-gray-300/80 my-4 rounded"/>
+        </View>
+        {/* User Details */}
+        <TouchableOpacity className="flex flex-row items-center ">
+          <View className="w-[93px] h-[91px] bg-[#d9d9d9] rounded-lg mr-5"></View>
+          <View>
+            <Text className="font-medium text-lg text-left">John Apple</Text>
+            <Text className="text-sm font-normal text-left text-gray-400">+123 456 678</Text>
+          </View>
+          <View className="absolute right-1">
+            <ChevronRightIcon height={16} width={10} strokeWidth={4} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="border-y border-gray-300/80 mt-4 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Payment Methods
+          </Text>
+        </TouchableOpacity>
+        {/* Css Note:  Border Bottom only  */}
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Address
+          </Text>
+        </TouchableOpacity>
+       
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            My Voucher
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Notification
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Language
+          </Text>
+        </TouchableOpacity>
+       
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Settings
+          </Text>
+        </TouchableOpacity>
+       
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Invite Friends
+          </Text>
+        </TouchableOpacity>
+       
+        <TouchableOpacity className="border-b border-gray-300/80 pt-3 pb-4">
+          <Text className="text-left text-xl font-medium">
+            Support
+          </Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity className="w-full py-2 rounded-md items-center bg-[#d9d9d9] mt-5 ">
+          <Text className="text-xl font-medium">Log Out</Text>
+        </TouchableOpacity>
+       
+      </SafeAreaView>
+    // </ScrollView>
   )
 }
 
